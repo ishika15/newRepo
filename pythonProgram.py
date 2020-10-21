b1efbef9776084b2bccbd1d5cb1d91f320fa0610
prachikakanodia2507 <73177603+prachikakanodia2507@users.noreply.github.com>
2020-10-21 21:33:21 +0530

    Update pythonProgram.py

--- pythonProgram.py
+++ pythonProgram.py
@@ -14,3 +14,28 @@ if num > 1:
          
 else:  
    print(num,"is not a prime number")
+    
+    # Program to add two matrices using nested loop 
+
+X = [[1,2,3], 
+	[4 ,5,6], 
+	[7 ,8,9]] 
+
+Y = [[9,8,7], 
+	[6,5,4], 
+	[3,2,1]] 
+
+
+result = [[0,0,0], 
+		[0,0,0], 
+		[0,0,0]] 
+
+# iterate through rows 
+for i in range(len(X)): 
+# iterate through columns 
+	for j in range(len(X[0])): 
+		result[i][j] = X[i][j] + Y[i][j] 
+
+for r in result: 
+	print(r) 
+
